5a0a8cdb6dd0e51343dbf97c16916fe2bd1c9b13
Thomas Kostas <tkostas75@gmail.com>
2019-07-01 15:55:50 +0900

    update
    Readme.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ros_launch_experiment_ws
+```
+source devel/setup.bash
+./start_chat.bash
+```
--- start_chat.bash
+++ start_chat.bash
@@ -1,5 +1,4 @@
 #! /bin/bash
-source .devel/setup.bash
 export CONF_FILE_PATH="./config.yaml"
 
 roslaunch ascent_car top_level_launcher.launch
